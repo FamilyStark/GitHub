3975be1b124a5d4445191f0ce5c3231e3bd08232
FamilyStark <boeitniet64@live.nl>
2014-09-10 10:59:15 -0700

    loo

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<div id="container">
+			<div id="head">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptatibus quia qui odio tempora veritatis deleniti laborum, eos voluptas, quidem culpa exercitationem ipsa aspernatur iusto ducimus eaque doloribus nisi repellendus.</div>
+			<div id="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae consectetur quo dolorum voluptates perferendis labore corporis, facilis animi commodi consequuntur deleniti vero, alias praesentium, vitae enim unde nesciunt inventore vel.</div>
+			<div id="foot">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa aspernatur at cupiditate, veniam blanditiis eius nam quidem debitis repudiandae totam, repellat eligendi laudantium cumque quibusdam vero asperiores esse unde obcaecati!</div>
+		</div>
+	</body>
+</html>
